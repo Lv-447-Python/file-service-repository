58216fcbb2870b759f8cff1d261a0e1ff4689fe0
Stanislav Hrytcyshyn <stashrutc@ukr.net>
2019-10-31 13:56:21 +0200

    Initial commit. Create project skeleton and initial requirements

---
+++ file_service/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app     = Flask(__name__)
+api     = Api(app)
+ma      = Marshmallow(app)
+db      = SQLAlchemy(app)
+migrate = Migrate(app, db)
