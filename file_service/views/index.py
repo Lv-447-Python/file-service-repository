aea35236119978c55d8aedabdede9dd1bb8e9130
Oleksandr Krynytskyi <sashakrinickijj@gmail.com>
2019-11-04 11:58:56 +0200

    <Oleksandr Krynytskyi> Add new feature: checking allowed formats.

--- file_service/views/index.py
+++ file_service/views/index.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename, redirect
 from flask.views import MethodView
 from flask import jsonify, request, render_template
 
-ALLOWED_EXTENSIONS = set(['csv', 'xls', 'xlsx'])
+ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
 
 
 def allowed_file(filename):
@@ -17,16 +17,14 @@ class FileLoading(MethodView):
         return render_template('index.html')
 
     def post(self):
-        # check if the post request has the file part
         if 'filename' not in request.files:
             return redirect(request.url)
         file = request.files['filename']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            return redirect(request.url)
-        file.save(os.path.join('.', file.filename))
-        return f'Uploaded {file.filename}'
+        if file.filename == '' or not allowed_file(file.filename):
+            return f'Fuck you.'
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('/home/alex/test', filename))
+        return f'Uploaded file {file.filename}'
 
     def __str__(self):
         return 'Class FileLoading - initilized'
