a56b98d932825b94479a1f183797af070d123924
Stanislav Hrytcyshyn <stashrutc@ukr.net>
2019-11-05 14:09:44 +0200

    <Stanislav Hrytcyshyn> Resolved file saving bug.

--- file_service/views/index.py
+++ file_service/views/index.py
@@ -1,15 +1,16 @@
 import os
 
-from file_service import app
+from file_service import app, db
+from file_service.models.file import File
 
 from werkzeug.utils import secure_filename, redirect
 from flask.views import MethodView
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, session
 
 import hashlib
 import datetime
 
-ALLOWED_EXTENSIONS = set(['csv', 'xls', 'xlsx'])
+ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
 
 
 def allowed_file(filename):
@@ -33,12 +34,21 @@ class FileLoading(MethodView):
             byte_line = file_content.read(piece_size)
         
         return sha256_hash.hexdigest()
+    
+    @staticmethod
+    def check_for_unique(file_hash):
+        '''Method to check if file with same hash code already exists'''
+        
         
         
         
     # TODO Create doc string for class description
 
     def get(self):
+
+        files = File.query.order_by(File.id)
+        for file in files:
+            print(file.file_name)
         return render_template('index.html')
 
     def post(self):
@@ -52,7 +62,7 @@ class FileLoading(MethodView):
 
         # if user does not select file, browser also
         # submit a empty part without filename
-        if file.filename == '':
+        if file.filename == '' or not allowed_file(file.filename):
             return redirect(request.url)
 
         file_size = len(file.read())
@@ -62,11 +72,13 @@ class FileLoading(MethodView):
 
         print('*' * 50)
         print('\tUploaded {0}\n\t File hash:{1}\n\t Loaded: {2}\n\t Size: {3}'.format(file.filename, loaded_file, datetime.datetime.now(), file_size))
+
+        file.stream.seek(0)
+
+        filename = secure_filename(file.filename)
         
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-
-
-
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
         return 'haha'
 
     def __str__(self):
