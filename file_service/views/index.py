c2c20be31484ae94fc1b8c1bdda6cec1c7223ee2
Stanislav Hrytcyshyn <stashrutc@ukr.net>
2019-11-07 19:28:55 +0200

    <Stanislav Hrytcyshyn> Refactored file loading logic. Fixed loading bug.

--- file_service/views/index.py
+++ file_service/views/index.py
@@ -45,29 +45,30 @@ class FileLoading(Resource):
     @staticmethod
     def generate_hash(file_content):
         try:
-            file_content = str(file_content)
+            check = str(file_content)
         except TypeError as e:
             print('File Content should be byte array string, \n', e)
             return
 
-        sha256_hash = hashlib.sha256()  # create instance of sha256 algorithm from hashlib
-        piece_size = 65536  # amount of bits which will be read for 1 iteration
-        byte_line = file_content.read(piece_size)  # read file content as bytes
+        sha256_hash = hashlib.sha256()               # create instance of sha256 algorithm from hashlib
+        piece_size  = 65536                          # amount of bits which will be read for 1 iteration
+        byte_line   = file_content.read(piece_size)  # read file content as bytes
 
-        while len(byte_line) > 0:  # means it's not the end of the file
-            sha256_hash.update(byte_line)  # update hash for every piece_size
+        while len(byte_line) > 0:                    # means it's not the end of the file
+            sha256_hash.update(byte_line)            # update hash for every piece_size
             byte_line = file_content.read(piece_size)  # update value
-        return sha256_hash.hexdigest()  # return hash value
+        return sha256_hash.hexdigest()                 # return hash value
 
     @staticmethod
     def check_for_unique(file_hash, file_size):
-        possible_files = [file for file in
+
+        possible_files = [file_instance for file_instance in
                           File.query.filter_by(file_size=file_size)]  # get all files with such file_size
 
         if len(possible_files) == 0:  # means this file is unique
             return True
         else:
-            possible_files_hashes = [file.file_hash for file in possible_files]  # get hashes of all possible files
+            possible_files_hashes = [file_instance.file_hash for file_instance in possible_files]  # get hashes of all possible files
             if binary_search(sorted(possible_files_hashes), file_hash):  # if we found file with such hash
                 exist_file = [existed for existed in File.query.filter_by(file_hash=file_hash)]  # get the existed file
                 print('//////////////////////////////////', exist_file[0])
@@ -76,46 +77,56 @@ class FileLoading(Resource):
                 return True  # file is unique
         print(possible_files_hashes)
 
-    # TODO Create doc string for class description
+
+    @staticmethod
+    def generate_meta(file_instance):
+        file_size = len(file_instance.read())
+        file_instance.stream.seek(0)
+        file_hash = FileLoading.generate_hash(file_instance)
+        file_instance.stream.seek(0)  # move pointer to the start of the file
+        filename = secure_filename(file_instance.filename)
+
+        file_path = ''
+
+        is_unique = FileLoading.check_for_unique(file_hash, file_size)
+        if is_unique == True:
+            file_path = app.config['UPLOAD_FOLDER'] + filename
+            file_instance.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        else:
+            file_path = is_unique
+
+        return [filename, file_size, file_hash, file_path]
+
     def get(self):
         return status.HTTP_200_OK
 
+
+
     def post(self):
+        print('in post')
         # check if the post request has the file part
-
-        file = request.files['File']
-        print(file)
-        return jsonify({'data': 'haha'})
-        
-
-        if 'filename' not in request.files:
+        if 'fileloading' not in request.files:
             return redirect(request.url)
-        file = request.files['filename']
+        file = request.files['fileloading']
+
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '' or not allowed_file(file.filename):
             return redirect(request.url)
 
-        file_size = len(file.read())
-        file.stream.seek(0)
-        file_hash = FileLoading.generate_hash(file)
-        file.stream.seek(0)  # move pointer to the start of the file
-        filename = secure_filename(file.filename)
+        meta = FileLoading.generate_meta(file)
 
-        file_path = ''
-
-        is_unique = FileLoading.check_for_unique(file_hash, file_size)
-        if is_unique:
-            file_path = app.config['UPLOAD_FOLDER'] + filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        else:
-            file_path = is_unique
-        input_file = File(filename, file_size, file_hash, file_path)
+        input_file = File(*meta)
 
         db.session.add(input_file)
         db.session.commit()
 
-        
+        return jsonify({
+            'meta': [element for element in meta]
+        })
+
+
+ 
 
     def __str__(self):
         return 'Class FileLoading - initilized'
--- file_service/views/routes.py
+++ file_service/views/routes.py
@@ -2,6 +2,5 @@ from file_service import app
 
 from file_service.views.index import FileLoading, api
 
-file_view = FileLoading.as_view('file_view')
 
 api.add_resource(FileLoading, '/')
