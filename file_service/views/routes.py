00e4b74efb1a24e79bd1da6d721c1028a91534a1
Oleksandr Krynytskyi <sashakrinickijj@gmail.com>
2019-11-15 14:15:04 +0200

    <Oleksandr Krynytskyi> Added new endpoint.

--- file_service/views/routes.py
+++ file_service/views/routes.py
@@ -1,15 +1,11 @@
-from file_service import app, api
+from file_service import api, app
 
 from file_service.views.index import FileLoading, FileInterface
 from file_service.views.file_filtering import FileFiltering
+from file_service.views.myviewer import MyViewer
 
 api.add_resource(FileLoading, '/')
 api.add_resource(FileInterface, '/file')
 api.add_resource(FileFiltering, '/filtering')
 
-# endpoint: /23 , /23/23/11
-
-
-# orest хоче брати від мене файл path, він бере цей файл потім бере відфільтровані рядочки від валентина , і генерує з того файл 
-
-# нам треба ендпоінт де ми будемо працювати з файлом тобто фільтрувати його, користувач працює з файлом, потім буде кнопка зберегти - і після цього запит летить в історію
+app.add_url_rule('/viewer', view_func=MyViewer.as_view('viewer'))
