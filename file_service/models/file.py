a1f1ab75bf93e99a50254103072220cbe8dad5d5
Oleksandr Krynytskyi <sashakrinickijj@gmail.com>
2019-11-25 18:26:44 +0200

    <Oleksandr Krynytskyi> Returned __init__ method.

--- file_service/models/file.py
+++ file_service/models/file.py
@@ -14,5 +14,11 @@ class File(DB.Model):
     file_hash = DB.Column(DB.Text, unique=False, nullable=False)
     file_path = DB.Column(DB.Text, unique=False, nullable=False)
 
+    def __init__(self, file_name, file_size, file_hash, file_path):
+        self.file_name = file_name
+        self.file_size = file_size
+        self.file_hash = file_hash
+        self.file_path = file_path
+
     def __repr__(self):
         return f'\t File: {self.file_name}'
