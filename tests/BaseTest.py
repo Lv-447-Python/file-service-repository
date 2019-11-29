2d914850400d8279343afed43fa0c8f825521e44
Oleksandr Krynytskyi <sashakrinickijj@gmail.com>
2019-11-29 23:44:34 +0200

    <Oleksandr Krynytskyi> Refactored unittest to let it work on makefile.

--- tests/BaseTest.py
+++ tests/BaseTest.py
@@ -1,3 +1,5 @@
+# NOTE: Don't run it via IDE, but on terminal!
+
 import io
 import json
 import unittest
@@ -18,7 +20,7 @@ class MyTestCase(unittest.TestCase):
         APP.config['TESTING'] = True
         APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-        APP.config['UPLOAD_FOLDER'] = 'upload_folder/'
+        APP.config['UPLOAD_FOLDER'] = 'tests/upload_folder/'
         self.APP = APP.test_client()
         DB.create_all()
 
@@ -31,19 +33,20 @@ class MyTestCase(unittest.TestCase):
         string = ","
         response = self.APP.post('/files',
                                  content_type='multipart/form-data',
-                                 data={'user_file': (io.BytesIO(test_data), 'test.csv')})
+                                 data={'user_file': (io.BytesIO(test_data), '/test.csv')})
         testing_data = json.loads(response.data)
         self.assertEqual(string.join(testing_data["filters"]), test_data.decode('utf-8'))
         self.assertEqual(response.status, '201 CREATED')
 
     def test_upload_file_using_file(self):
-        with open('test_files/Test_dataset_filterMe.csv', 'rb') as file_test:
+        print(os.getcwd())
+        with open('tests/test_files/Test_dataset_filterMe.csv', 'rb') as file_test:
             binary_file_test = file_test.read()
 
         with io.BytesIO(binary_file_test) as testing_file:
             response = self.APP.post('/files',
                                      content_type='multipart/form-data',
-                                     data={'user_file': (testing_file, 'Test_dataset_filterMe.csv')})
+                                     data={'user_file': (testing_file, '/Test_dataset_filterMe.csv')})
         self.assertEqual(response.status, '201 CREATED')
 
     def test_find_file(self):
@@ -55,12 +58,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(response.status, '200 OK')
 
     def test_delete_file(self):
-        with open('test_files/Test_dataset_filterMe.csv', 'rb') as file_test:
+        with open('tests/test_files/Test_dataset_filterMe.csv', 'rb') as file_test:
             binary_test_file = file_test.read()
         with io.BytesIO(binary_test_file) as testing_file:
             response = self.APP.post('/files',
                                      content_type='multipart/form-data',
-                                     data={'user_file': (testing_file, 'file_for_delete_from_db.csv')})
+                                     data={'user_file': (testing_file, '/file_for_delete_from_db.csv')})
         self.assertEqual(response.status, '201 CREATED')
         response_delete = self.APP.delete('/file/' + str(json.loads(response.data)['data']['id']))
         self.assertEqual(response_delete.status, '200 OK')
